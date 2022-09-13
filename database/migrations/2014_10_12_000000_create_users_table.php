a5e32b0ef7586b1fd6f0b04189493814523fc831
Eluney Salvaro <eluneyjsalvaro@gmail.com>
2022-09-13 16:48:21 -0300

    migraciones cambiadas

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,14 +17,17 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->string('surname')->nullable();
+            $table->date('birthday')->nullable();
             $table->string('email')->unique();
+            $table->string('dni')->nullable();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password')->nullable();
             $table->foreignId('role_id')->constrained();
             $table->foreignIdFor(Ciudad::class)->nullable();
             $table->integer('points'); 
+            $table->string('telephone_number')->nullable();
             $table->rememberToken();
-            $table->foreignId('current_team_id')->nullable();
             $table->string('profile_photo_path', 2048)->nullable();
             $table->string('external_id')->nullable();
             $table->timestamps();
