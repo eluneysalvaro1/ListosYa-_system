9525837b79a00cbe1b1d5d3572a93427daf88711
Juan Julian Mora <jota@Juans-MacBook-Air.local>
2022-10-17 16:11:26 -0300

    ahora si

--- resources/views/livewire/program-search.blade.php
+++ resources/views/livewire/program-search.blade.php
@@ -5,9 +5,12 @@
         <div class="min-w-screen  bg-gray-100 flex items-center justify-center bg-gray-100 font-sans overflow-hidden">
             <div class="w-auto mx-10 ">
                 <div class="flex justify-end pt-5 pr-10">
-                    <div style="display: flex; align-items: center; margin-top: 20px">
-                        <span>Buscar: </span>
-                        <input wire:model="busqueda" style="width:30%; height: 20px; margin-left: 10px" type="text">
+                    <div class="relative mt-1">
+                        <div class="flex absolute inset-y-0 left-0 items-center pl-3 pointer-events-none">
+                            <svg class="w-5 h-5 text-gray-500 dark:text-gray-400" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" clip-rule="evenodd"></path></svg>
+                        </div>
+                        
+                        <input type="text" class="block p-2 pl-10 w-80 text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="ingrese el nombre del programa" wire:model="busqueda" />
                     </div>
                     <button
                         class=" float-right block focus:outline-none text-white  bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-2 py-1 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-green-900 "
