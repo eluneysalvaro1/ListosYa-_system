f13f890582abb89335b2bb5f2139f5526e60e43d
jotanqn <jjulian.mora@est.fi.uncoma.edu.ar>
2022-11-16 18:32:53 -0300

    rama nueva

---
+++ app/Http/Controllers/CheckAttendanceController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\CheckAttendance;
+use Illuminate\Http\Request;
+
+class CheckAttendanceController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\CheckAttendance  $checkAttendance
+     * @return \Illuminate\Http\Response
+     */
+    public function show(CheckAttendance $checkAttendance)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\CheckAttendance  $checkAttendance
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(CheckAttendance $checkAttendance)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\CheckAttendance  $checkAttendance
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, CheckAttendance $checkAttendance)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\CheckAttendance  $checkAttendance
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(CheckAttendance $checkAttendance)
+    {
+        //
+    }
+}
